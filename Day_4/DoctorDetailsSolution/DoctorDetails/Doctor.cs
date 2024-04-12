e7e7550d14bc6ac96695c11adb31baee2b2decf4
Sukesh <78144062+sukesh-n@users.noreply.github.com>
2024-04-12 21:06:32 +0530

    Doctor_class

---
+++ Day_4/DoctorDetailsSolution/DoctorDetails/Doctor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorDetails
+{
+    class Doctor
+    {
+        public int DoctorId {  get; set; }
+        public string Name {  get; set; }
+        public int Age { get; set; }
+        public int Experience { get; set; }
+        public string Qualification {  get; set; }
+        public string Speciality {  get; set; }
+        public Doctor() {
+            DoctorId = 0;
+            Name=string.Empty;
+            Age = 0;
+            Experience = 0;
+            Qualification = string.Empty;
+            Speciality = string.Empty;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="doctorId">"Enter doctor ID as Integer"</param>
+        /// <param name="name">Enter doctor name in string</param>
+        /// <param name="age">Enter doctor age as Integer</param>
+        /// <param name="experience">Enter doctor experience as Integer</param>
+        /// <param name="qualification">Enter doctor qualification as Integer</param>
+        /// <param name="speciality">Enter doctor speciality as Integer</param>
+        public Doctor(int doctorId, string name, int age, int experience, string qualification, string speciality)
+        {
+            DoctorId = doctorId;
+            Name = name;
+            Age = age;
+            Experience = experience;
+            Qualification = qualification;
+            Speciality = speciality;
+        }
+        public void PrintDoctorDetails()
+        {
+            Console.WriteLine($"Doctor ID\t:\t{DoctorId}");
+            Console.WriteLine($"Doctor Name\t:\t{Name}");
+            Console.WriteLine($"Doctor Age\t:\t{Age}");
+            Console.WriteLine($"Doctor Name\t:\t{Experience}");
+            Console.WriteLine($"Doctor ID\t:\t{Qualification}");
+            Console.WriteLine($"Doctor Name\t:\t{Speciality}");
+        }
+    }
+}
---
+++ Day_4/DoctorDetailsSolution/DoctorDetails/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DoctorDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doctor details");
+            Doctor first = new Doctor();
+
+            Console.WriteLine("Enter ID of doctor");
+            first.DoctorId=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter name of the doctor");
+            first.Name = Console.ReadLine();
+            Console.WriteLine("Enter age of doctor");
+            first.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter experience of doctor");
+            first.Experience = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter speciality of doctor");
+            first.Speciality = Console.ReadLine();
+            Console.WriteLine("Enter qualification of doctor");
+            first.Qualification = Console.ReadLine();
+            first.PrintDoctorDetails();
+            
+        }
+    }
+}
