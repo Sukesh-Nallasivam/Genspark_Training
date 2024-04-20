02e8451157e75fcb8eef6a8adefe4b13ec1ddeb8
sukesh-n <sukeshtn.2@gmail.com>
2024-04-21 00:03:29 +0530

    Day8UpdatedWithServicesProgram

---
+++ Day8/ClinicBLLLibrary/IAppointment.cs
@@ -0,0 +1,18 @@
+﻿using ClinicModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicBLLLibrary
+{
+    public interface IAppointmentService
+    {
+        void AddAppointment(Appointment appointment);
+        void UpdateAppointment(Appointment appointment);
+        void DeleteAppointment(int id);
+        Appointment GetAppointmentById(int id);
+        
+    }
+}
---
+++ Day8/ClinicRepositoryDAL/PatientRepositoryClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClinicModelLibrary;
+namespace ClinicDALLibrary
+{
+    internal class PatientRepositoryClass : IRepository<int, Patient>
+    {
+        readonly Dictionary<int, Patient> _patients;
+        public PatientRepositoryClass()
+        {
+            _patients = new Dictionary<int, Patient>();
+        }
+        int GeneratePatientId()
+        {
+            if (_patients.Count == 0)
+            {
+                return 1;
+            }
+            int id = _patients.Keys.Max();
+            return ++id;
+        }
+        public Patient Add(Patient item)
+        {
+            if (_patients.ContainsValue(item))
+            {
+                return null;
+            }
+            item.Id = GeneratePatientId();
+
+            _patients.Add(item.Id, item);
+            return item;
+        }
+
+        public Patient Delete(int key)
+        {
+            if (_patients.ContainsKey(key))
+            {
+                var appoinment = _patients[key];
+                _patients.Remove(key);
+                return appoinment;
+            }
+            return null;
+        }
+
+        public Patient Get(int key)
+        {
+            return _patients.ContainsKey(key) ? _patients[key] : null;
+        }
+
+        public List<Patient> GetAll()
+        {
+            if (_patients.Count == 0)
+                return null;
+            return _patients.Values.ToList();
+        }
+
+        public Patient Update(Patient item)
+        {
+            if (_patients.ContainsKey(item.Id))
+            {
+                _patients[item.Id] = item;
+                return item;
+            }
+            return null;
+        }
+    }
+}
