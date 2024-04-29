f307e2851b4509a0370e7a9045044eb9d36891a4
sukesh-n <sukeshtn.2@gmail.com>
2024-04-29 21:31:11 +0530

    ExcelSheetColumnTitleSolution

---
+++ Day14/ExcelSheetCoulumnTitleSolution/ExcelSheetCoulumnTitle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExcelSheetCoulumnTitle
+{
+    internal class Program
+    {
+           public async Task ConvertToTitle(int columnNumber)
+            {
+                int num = columnNumber;
+                int div, rem;
+                string result = "";
+                
+                while (num > 0)
+                {
+                    num--;
+                    div = (num / 26);
+                    rem = (num % 26);
+                    int temp = 65 + rem;
+                    result = ((char)temp).ToString() + result;
+                    num = div;
+
+                }
+                Console.WriteLine(columnNumber);
+                await Task.Delay(10);
+                Console.WriteLine(result);
+            }
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            Random rnd = new Random();
+            await program.ConvertToTitle(rnd.Next());
+        }
+    }
+}
