f6ed1f93090006b4afc8074a7b45c11717e40876
sukesh-n <sukeshtn.2@gmail.com>
2024-05-03 10:53:52 +0530

    CorelatedSubQuery

--- Day17/Day_17_Training.sql
+++ Day17/Day_17_Training.sql
@@ -13,3 +13,52 @@ select * from Suppliers
 select ProductName from products where SupplierId=
 (select SupplierID from suppliers where CompanyName = 'Tokyo Traders')
 
+--get all the products that are supplied by suppliers from USA
+-- "=" wont work when there is multiple values
+select ProductName from products where SupplierID in 
+(select SupplierID from Suppliers where Country = 'USA')
+
+--get all the products from the category that has 'ea' in the description
+select * from Categories
+select ProductName from Products where CategoryID in 
+(select CategoryID from Categories where Description like '%ea%')
+
+
+
+select * from products where CategoryID in
+(select CategoryID from Categories where Description like '%ea%')
+and SupplierID = 
+(select SupplierID from Suppliers where CompanyName = 'Tokyo Traders')
+
+
+select * from customers
+select * from orders
+select * from [Order Details]
+--select the product id and the quantity ordered by customrs from France
+select productID,Quantity from [Order Details] where OrderID in
+(select OrderID from Orders where CustomerID in 
+(select CustomerID from Customers where Country = 'France'))
+
+--Get the products that are priced above the average price of all the products
+
+select * from products
+
+select avg(unitPrice) from Products
+
+select productName,UnitPrice from products where UnitPrice > 
+(select avg(UnitPrice) from products)
+---Co-related sub query
+--select latest order by every employee
+select * from Orders
+
+select * from Orders a
+where OrderDate = (select max(orderDate) from Orders as b where a.EmployeeID=b.EmployeeID)
+order by a.EmployeeID
+
+
+--Select the maximum priced product in every category
+
+select * from products
+select * from products a where UnitPrice = 
+(select max(UnitPrice) from Products b where a.CategoryID=b.CategoryID)
+order by a.CategoryID
