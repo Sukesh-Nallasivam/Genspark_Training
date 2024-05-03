53076a21a89cc8fa734e6b3d525338073b9b6d5e
sukesh-n <sukeshtn.2@gmail.com>
2024-05-03 13:08:13 +0530

    Joins

--- Day17/Day_17_Training.sql
+++ Day17/Day_17_Training.sql
@@ -62,3 +62,97 @@ select * from products
 select * from products a where UnitPrice = 
 (select max(UnitPrice) from Products b where a.CategoryID=b.CategoryID)
 order by a.CategoryID
+
+
+--select the order number for the maximum fright paid by every customer
+
+select * from Orders
+select OrderID,CustomerID,Freight from Orders a where Freight =
+(select max(freight) from Orders b where a.CustomerID=b.CustomerID)
+order by a.CustomerID
+
+
+--cross join 
+
+select * from Customers,Categories
+
+--Inner join
+select * from categories where CategoryID 
+not in (select distinct categoryid from products)
+
+select * from Suppliers where SupplierID 
+not in (select distinct SupplierID from products)
+
+--Get teh categoryId and teh productname
+select CategoryId,ProductName from products
+
+--Get teh categoryname and the productname
+select categoryName,ProductName from Categories 
+join Products on Categories.CategoryID = Products.CategoryID
+
+
+--Get the Supplier company name, contact person name, Product name and the stock ordered
+select *  from products
+select * from Suppliers
+select * from [order Details]
+
+select CompanyName,contactName,productName,UnitsOnOrder from Suppliers
+join products on Suppliers.SupplierID=Products.SupplierID
+order by UnitsOnOrder desc
+
+
+ --Print the order id, customername and the fright cost for all teh orders
+select * from orders
+select * from Customers
+
+select orderId,orders.CustomerID,ContactName,Freight from orders
+join Customers on orders.CustomerID=Customers.CustomerID
+
+
+--product name, quantity sold, Discount Price, Order Id, Fright for all orders
+ select o.OrderID "Order ID",p.Productname, od.Quantity "Quantity Sold",
+ (p.UnitPrice*od.Quantity) "Base Price", 
+ ((p.UnitPrice*od.Quantity)-((p.UnitPrice*od.Quantity)* od.Discount/100)) "Discounted price",
+ Freight "Freight Charge"
+ from Orders o join [Order Details] od
+ on o.OrderID = od.OrderID
+ join Products p on p.ProductID = od.ProductID
+ order by o.OrderID
+
+
+
+ --select customer name, product name, quantity sold and the frieght, 
+ --total price(unitpice*quantity+freight)
+select * from customers
+select * from products
+select * from suppliers
+select * from orders
+select * from [Order Details]
+
+
+select d.ContactName "Customer Name",b.ProductName "Product Name",c.Freight,a.Quantity "Units Sold"
+,(b.UnitPrice*c.Freight+a.Quantity) "total price"
+from [Order Details] a join Products b on a.ProductID=b.ProductID
+join orders c on c.OrderID=a.OrderID
+join Customers d on d.CustomerID=c.CustomerID
+join Suppliers e on e.SupplierID=b.SupplierID
+order by d.ContactName
+
+
+
+ --Print the product name and the total quantity sold
+select productName,sum(quantity) "Total quantity sold"
+from products p join [Order Details] od
+on p.ProductID = od.ProductID
+group by ProductName
+order by 2 desc
+
+
+--emp name ,custname,prod name,total price
+
+
+select ( a.FirstName+' '+a.LastName) "Employee Name",c.ContactName,e.ProductName,(e.UnitPrice*b.Freight+d.Quantity) "total price"
+from Employees a join Orders b on a.EmployeeID=b.EmployeeID
+join Customers c on b.CustomerID=b.CustomerID
+join [Order Details] d on d.OrderID=b.OrderID
+join Products e on e.ProductID=d.ProductID
